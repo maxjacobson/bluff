df55ebcc433f76fd5dc73974d9175b236c0c1814
Max Jacobson <max@hardscrabble.net>
2020-04-23 05:37:47 -0400

    Remove references to service workers
    I'm not planning to use this. Offline support is a nice idea, but it's
    an online game. And it will make it more complicated than I can
    personally handle.

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,5 @@
 import "./main.css";
 import { Elm } from "./Main.elm";
-import * as serviceWorker from "./serviceWorker";
 
 Elm.Main.init({
   node: document.getElementById("root"),
@@ -8,8 +7,3 @@ Elm.Main.init({
     apiRoot: process.env.ELM_APP_API_ROOT || "http://localhost:3000",
   },
 });
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
