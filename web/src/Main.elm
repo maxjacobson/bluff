fb0be4116f6369eead7c7d488aab9e5e0d1cc240
Max Jacobson <max@hardscrabble.net>
2020-04-22 23:33:02 -0400

    Add basic routing and GamePage

--- web/src/Main.elm
+++ web/src/Main.elm
@@ -23,17 +23,53 @@ main =
 type alias Model =
     { key : Nav.Key
     , url : Url.Url
-    , gameID : String
+    , gameID : Maybe GameID
+    , currentPage : Page
     }
+
+
+type alias GameID =
+    String
+
+
+type Page
+    = HomePage
+    | GamePage GameID
+    | NotFound
 
 
 type alias Flags =
     ()
 
 
+pageFromURL : Url.Url -> Page
+pageFromURL url =
+    case url.path of
+        "/" ->
+            HomePage
+
+        _ ->
+            case gameIDFromUrl url of
+                Just gameID ->
+                    GamePage gameID
+
+                _ ->
+                    NotFound
+
+
+gameIDFromUrl : Url.Url -> Maybe GameID
+gameIDFromUrl url =
+    case String.dropLeft 1 url.path of
+        "" ->
+            Nothing
+
+        anything ->
+            Just anything
+
+
 init : Flags -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init _ url key =
-    ( Model key url "", Cmd.none )
+    ( Model key url (gameIDFromUrl url) (pageFromURL url), Cmd.none )
 
 
 type Msg
@@ -47,10 +83,15 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         UpdatedGameID newGameId ->
-            ( { model | gameID = newGameId }, Cmd.none )
+            ( { model | gameID = Just newGameId }, Cmd.none )
 
         SubmittedGoToGame ->
-            ( model, Cmd.none )
+            case model.gameID of
+                Just gameID ->
+                    ( model, Nav.pushUrl model.key ("/" ++ gameID) )
+
+                Nothing ->
+                    ( model, Cmd.none )
 
         UrlRequested urlRequest ->
             case urlRequest of
@@ -61,7 +102,11 @@ update msg model =
                     ( model, Nav.load href )
 
         UrlChanged url ->
-            ( { model | url = url }
+            let
+                newPage =
+                    pageFromURL url
+            in
+            ( { model | url = url, currentPage = newPage }
             , Cmd.none
             )
 
@@ -72,18 +117,30 @@ subscriptions _ =
 
 
 view : Model -> Browser.Document Msg
-view _ =
+view model =
     { title = "Bluff"
     , body =
         [ div []
             [ h1 [] [ text "Bluff" ]
-            , p []
-                [ text "Bluff is a poker game for bluffers. Enter your group's game ID to proceed."
-                ]
-            , form [ onSubmit SubmittedGoToGame ]
-                [ input [ attribute "type" "text", attribute "placeholder" "Your group's game ID", onInput UpdatedGameID ] []
-                , input [ attribute "type" "submit", attribute "value" "Go" ] []
-                ]
+            , case model.currentPage of
+                HomePage ->
+                    div []
+                        [ p [] [ text "Bluff is a poker game for bluffers. Enter your group's game ID to proceed." ]
+                        , form [ onSubmit SubmittedGoToGame ]
+                            [ input [ attribute "type" "text", attribute "placeholder" "Your group's game ID", onInput UpdatedGameID ] []
+                            , input [ attribute "type" "submit", attribute "value" "Go" ] []
+                            ]
+                        ]
+
+                GamePage gameID ->
+                    div []
+                        [ p []
+                            [ text ("You are on the game page for game ID: " ++ gameID)
+                            ]
+                        ]
+
+                NotFound ->
+                    div [] [ text "Page not found" ]
             ]
         ]
     }
