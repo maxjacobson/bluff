a30b5e933315e3e2979a3d84c4887ea65207a362
Max Jacobson <max@hardscrabble.net>
2020-04-24 02:31:49 -0400

    Small refactor for the Nickname stuff
    I decided the `class << self` thing looked too janky and I didn't want
    it.

--- api/app/models/human.rb
+++ api/app/models/human.rb
@@ -2,21 +2,14 @@
 
 # A human who visited the site, and may or may not have played a game
 class Human < ApplicationRecord
-  class << self
-    def recognize(uuid)
-      return if uuid.blank?
+  # Even if we don't know who they are, we're going to act like we do
+  def self.recognize(uuid)
+    return if uuid.blank?
 
-      find_by_uuid(uuid) || create!(
-        uuid: uuid,
-        nickname: random_nickname
-      )
-    end
-
-    private
-
-    def random_nickname
-      [Faker::Hipster.word, Faker::Creature::Animal.name].join(' ').titlecase
-    end
+    find_by_uuid(uuid) || create!(
+      uuid: uuid,
+      nickname: RandomNickname.new.to_s
+    )
   end
 
   def record_heartbeat(game)
